b3b9abeb29ce7fb39d9c762ef9e03ca3f33254f1
nestorsdelgado <nestorsdelgado@gmail.com>
2025-02-26 17:42:56 +0000

    Lab solution - Nestor

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
 
 const app = express();
 
@@ -12,6 +14,12 @@ app.use(express.json());
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
 
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+
+mongoose
+    .connect(MONGODB_URI)
+    .then((x) => console.log(`Connected to Mongo! Database name: ${x.connections}`))
+    .catch((err) => console.error("Error connecting to mongo", err))
 
 
 // ROUTES
@@ -23,23 +31,77 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post("/recipes", (req, res) => {
+    Recipe.create({
+        title: req.body.title,
+        instructions: req.body.instructions,
+        level: req.body.level,
+        ingredients: req.body.ingredients,
+        image: req.body.image,
+        duration: req.body.duration,
+        isArchived: req.body.isArchived,
+        created: req.body.created
+    })
+        .then((createdRecipe) => {
+            console.log("Recipe created ->", createdRecipe);
+            res.status(201).json(createdRecipe);
+        })
+        .catch((error) => {
+            console.error("Error while creating the recipe ->", error);
+            res.status(500).json({ error: "Failed to create the recipe" });
+        });
+});
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
+app.get("/recipes", (req, res) => {
+    Recipe.find({})
+        .then((allRecipes) => {
+            console.log("Retrieved recipes ->", allRecipes);
 
+            res.status(200).json(allRecipes);
+        })
+        .catch((error) => {
+            console.error("Error while retrieving recipes ->", error);
+            res.status(500).json({ error: "Failed to retrieve recipes" });
+        });
+});
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
+app.get("/recipes/:id", (req, res) => {
+    Recipe.findById(req.params.id)
+        .then((recipe) => {
+            res.status(200).json(recipe)
+        })
+        .catch((error) => {
+            res.status(500).json({ message: "Error while getting a single recipe" })
+        })
+})
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
+app.get("/recipes/:id", (req, res) => {
+    Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((updatedRecipe) => {
+            res.status(200).json(updatedRecipe)
+        })
+        .catch((error) => {
+            res.status(500).json({ message: "Error while updating a single recipe" })
+        })
+})
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-
+app.get("/recipes/:id", (req, res) => {
+    Recipe.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.status(204).send()
+        })
+        .catch((error) => {
+            res.status(500).json({ message: "Error while deleting a single recipe" })
+        })
+})
 
 
 // Start the server
